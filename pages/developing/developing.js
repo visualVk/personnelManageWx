556422504439be255e97397b36832243ed87d48d
Tom <2048137831@qq.com>
2019-11-05 13:05:51 +0800

    添加： 1. 合同显示页面 2. 发展信息显示页面

---
+++ pages/developing/developing.js
@@ -0,0 +1,119 @@
+// pages/developing/developing.js
+import * as developingApi from '../../utils/Developing/api/DevelopingAPi'
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    open: false,
+    list: [
+      {
+        open: false,
+        developing: {
+          developingId: 1,
+          personId: wx.getStorageSync('personId'),
+          learnExperience: '12313',
+          jobExperience: 'xxxx',
+          date: '2019-10-24 23:45:11',
+        }
+      }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    developingApi.findDevelopingsByPersonId().then(res => {
+      let list = res.queryResult.list
+      for (let i = 0, len = list.length; i < len; i++) {
+        this.data.list[i] = {
+          open: false,
+          developing: list[i]
+        }
+      }
+      let that = this
+      this.setData({
+        list: that.data.list
+      })
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  tap_ch: function (e) {
+    if (this.data.open) {
+      this.setData({
+        open: false
+      });
+    } else {
+      this.setData({
+        open: true
+      });
+    }
+  },
+  widgetsToggle: function (e) {
+    let list = this.data.list;
+    let id = e.currentTarget.id;
+    for (let i = 0, len = list.length; i < len; i++) {
+      let tmp = list[i];
+      if (tmp.developing.developingId == id) {
+        list[i].open = !list[i].open
+      }
+    }
+    this.setData({list: list})
+  },
+  go2Page: function (e) {
+    wx.redirectTo({
+      url: '/pages/' + e.target.dataset.val
+    })
+  }
+})
---
+++ pages/pact/pact.js
@@ -0,0 +1,125 @@
+// pages/pact/pact.js
+import * as pactApi from '../../utils/Pact/api/PactApi'
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    open: false,
+    list: [
+      {
+        open: false,
+        pact: {
+          pactId: 1,
+          pactName: 1,
+          pactType: {
+            pactTypeId: 1,
+            pactTypeName: '奖励金'
+          },
+          pactContent: 'xxxx',
+          date: '2019-10-24 23:45:11',
+          duration: 12,
+          salary: '12123'
+        }
+      }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    pactApi.findAllPactByPersonId().then(res => {
+      let list = res.queryResult.list;
+      // debugger
+      for (let i = 0, len = list.length; i < len; i++) {
+        this.data.list[i] = {
+          open: false,
+          pact: list[i]
+        }
+      }
+      this.setData({
+        list: this.data.list
+      })
+      // debugger
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  tap_ch: function (e) {
+    if (this.data.open) {
+      this.setData({
+        open: false
+      });
+    } else {
+      this.setData({
+        open: true
+      });
+    }
+  },
+  widgetsToggle: function (e) {
+    let list = this.data.list;
+    let id = e.currentTarget.id;
+    for (let i = 0, len = list.length; i < len; i++) {
+      let tmp = list[i];
+      if (tmp.pact.pactId == id) {
+        list[i].open = !list[i].open
+      }
+    }
+    this.setData({list: list})
+  },
+  go2Page: function (e) {
+    wx.redirectTo({
+      url: '/pages/' + e.target.dataset.val
+    })
+  },
+})
